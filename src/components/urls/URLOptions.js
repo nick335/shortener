cadcdf2781da204c49e323d30f6207deb738758d
[nick335] <[chigozieerigo@gmail.com]>
2023-10-13 23:14:12 +0100

    options modal fixed!

--- src/components/urls/URLOptions.js
+++ src/components/urls/URLOptions.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function URLOptions({ isOpen }) {
   return (
-    <ul className={` ${ isOpen ? 'block z-10' : 'hidden' } absolute top-[1rem] rounded-[0.625rem] shadow right-0 bg-ecx-white w-[5.625rem] p-[0.62rem] flex flex-col gap-y-[0.62rem] font-inter text-small`}>
-     <li>Edit</li>
-     <li>Share URL</li>
-     <li className='text-ruby'>Delete</li>
+    <ul className={` ${ isOpen ? 'block z-10' : 'hidden' } absolute top-[1rem] rounded-[0.625rem] shadow right-0 bg-ecx-white min-w-[5.825rem] p-[0.62rem] flex flex-col gap-y-[0.62rem] font-inter text-small`}>
+     <li className='cursor-pointer'>Edit</li>
+     <li className='cursor-pointer'>Share URL</li>
+     <li className='text-ruby cursor-pointer'>Delete</li>
     </ul>
   )
 }
